dfa475fc73564ce978de3462e1d8b8b975bb98c4
Caio Carvalho <kaiogesteira@gmail.com>
2023-05-08 16:51:25 -0300

    Fixes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes/index.routes.js";
+import dotenv from "dotenv";
 
 // Server configuration
 const app = express();
-const PORT = 5000;
 
 // App Configs
 app.use(cors());
 app.use(express.json());
 app.use(router);
+dotenv.config();
 
+const PORT = process.env.port || 5000;
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
